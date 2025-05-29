e5391d0c8e971dc1fcb32632b0e4241c895c40b6
Uzan Latif <69574011+uzanlatif@users.noreply.github.com>
2025-05-29 15:26:44 +0900

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { User } from 'lucide-react';
 import MetaLogo from '../assets/meta_logo-2-removebg-preview.png';
+import { Capacitor } from '@capacitor/core';
 
 const Navbar: React.FC = () => {
   const ip = import.meta.env.VITE_IP_ADDRESS;
+  const isNative = Capacitor.isNativePlatform(); // true jika di APK/iOS
 
   return (
     <header
       className="bg-gray-900 shadow-sm border-b border-gray-800"
-      style={{ paddingTop: 'env(safe-area-inset-top)' }}
+      style={{
+        paddingTop: isNative ? '0px' : 'env(safe-area-inset-top, 0px)',
+        minHeight: '56px', // tetap jaga tinggi minimum
+      }}
     >
       <div className="flex items-center justify-between px-4 py-3">
         {/* Kiri: Logo dan Judul */}
@@ -17,7 +22,7 @@ const Navbar: React.FC = () => {
             src={MetaLogo}
             alt="Logo"
             className="mr-3 max-h-10"
-            style={{ height: 'auto', width: 'auto', padding:8}}
+            style={{ height: 'auto', width: 'auto', padding: 8 }}
           />
         </div>
 
