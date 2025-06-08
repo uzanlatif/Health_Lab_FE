085de2732f8a70d7ee10ed8c088e01486b3972d1
Uzan Latif <69574011+uzanlatif@users.noreply.github.com>
2025-06-08 14:20:58 +0900

    add dist for electron

---
+++ electron.js
@@ -0,0 +1,18 @@
+// electron.js
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    webPreferences: {
+      contextIsolation: true, // security best practice
+    },
+  });
+
+  // Load Vite-built frontend from `dist/index.html`
+  win.loadFile(path.join(__dirname, 'dist/index.html'));
+}
+
+app.whenReady().then(createWindow);
