50f9e408eb2146393a4f87c86b28558f31f7ced0
Latif Uzan <uzanlatif@example.com>
2025-06-11 09:07:08 +0900

    new electron

--- electron.cjs
+++ electron.cjs
@@ -1,12 +1,14 @@
 // electron.js
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, globalShortcut } = require('electron');
 const path = require('path');
 
 app.disableHardwareAcceleration();
 
+let win;
+
 function createWindow() {
-  const win = new BrowserWindow({
-    frame: true, // tampilkan title bar agar bisa drag / kontrol
+  win = new BrowserWindow({
+    fullscreen: true, // Set jendela fullscreen
     webPreferences: {
       contextIsolation: true,
     },
@@ -15,9 +17,18 @@ function createWindow() {
   win.loadFile(path.join(__dirname, 'dist/index.html'));
 
   win.once('ready-to-show', () => {
-    win.maximize(); // hanya maximize, bukan fullscreen
     win.show();
+  });
+
+  // Daftar shortcut Ctrl+Q untuk keluar aplikasi
+  globalShortcut.register('CommandOrControl+Q', () => {
+    app.quit();
   });
 }
 
 app.whenReady().then(createWindow);
+
+// Unregister semua shortcut saat app keluar
+app.on('will-quit', () => {
+  globalShortcut.unregisterAll();
+});
